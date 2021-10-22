a22999656ecc87c5727ab285a9191e95860f691e
ZhangJY0426 <atguigu0426@gg.com>
2021-10-22 16:09:24 +0800

    day01-下午

--- gmall-logger/src/main/java/com/atguigu/gmalllogger/controller/LoggerConotroller.java
+++ gmall-logger/src/main/java/com/atguigu/gmalllogger/controller/LoggerConotroller.java
@@ -4,6 +4,8 @@ package com.atguigu.gmalllogger.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +15,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class LoggerConotroller {
 
 //    Logger logger = LoggerFactory.getLogger(LoggerConotroller.class);
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
 
     @RequestMapping("test")
     public String test(){
@@ -36,6 +41,9 @@ public class LoggerConotroller {
 //       logger.info();
         //将日志打印到控制台并落盘
         log.info(jsonStr);
+
+        //将数据发送至Kafka
+        kafkaTemplate.send("ods_base_log", jsonStr);
 
         return "success";
     }
