d44d42e734ad089c17694127f4902212da69a2de
ZhangJY0426 <atguigu0426@gg.com>
2021-10-22 11:12:12 +0800

    day01-上午

---
+++ gmall-logger/src/main/java/com/atguigu/gmalllogger/controller/LoggerConotroller.java
@@ -0,0 +1,43 @@
+package com.atguigu.gmalllogger.controller;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class LoggerConotroller {
+
+//    Logger logger = LoggerFactory.getLogger(LoggerConotroller.class);
+
+    @RequestMapping("test")
+    public String test(){
+        System.out.println("111111");
+        return "success";
+    }
+
+    @RequestMapping("test1")
+    public String test1(@RequestParam("name") String name,
+                        @RequestParam("age") Integer age
+                        ){
+        System.out.println("123");
+        return "name:" + name + "age:" + age;
+    }
+
+    @RequestMapping("applog")
+    public String getLogger(@RequestParam("param") String jsonStr){
+
+//        System.out.println(jsonStr);
+
+//       logger.info();
+        //将日志打印到控制台并落盘
+        log.info(jsonStr);
+
+        return "success";
+    }
+
+}
