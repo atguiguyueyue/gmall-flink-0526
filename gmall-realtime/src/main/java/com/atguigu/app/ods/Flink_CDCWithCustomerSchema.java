ee97d4f7523caf92a1cd5fa433ec34edd6092ae2
ZhangJY0426 <atguigu0426@gg.com>
2021-10-23 11:35:51 +0800

    day01-上午

---
+++ gmall-realtime/src/main/java/com/atguigu/app/ods/Flink_CDCWithCustomerSchema.java
@@ -0,0 +1,51 @@
+package com.atguigu.app.ods;
+
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.ververica.cdc.connectors.mysql.MySQLSource;
+import com.alibaba.ververica.cdc.connectors.mysql.table.StartupOptions;
+import com.alibaba.ververica.cdc.debezium.DebeziumDeserializationSchema;
+import com.alibaba.ververica.cdc.debezium.DebeziumSourceFunction;
+import com.atguigu.bean.MyDeseriali;
+import com.atguigu.utils.MyKafkaUtil;
+import io.debezium.data.Envelope;
+import org.apache.flink.api.common.typeinfo.BasicTypeInfo;
+import org.apache.flink.api.common.typeinfo.TypeInformation;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer;
+import org.apache.flink.util.Collector;
+import org.apache.kafka.connect.data.Field;
+import org.apache.kafka.connect.data.Schema;
+import org.apache.kafka.connect.data.Struct;
+import org.apache.kafka.connect.source.SourceRecord;
+
+import java.util.List;
+
+public class Flink_CDCWithCustomerSchema {
+    public static void main(String[] args) throws Exception {
+        //1.获取流的执行环境
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        env.setParallelism(1);
+
+        //设置Mysql的相关属性
+        DebeziumSourceFunction<String> sourceFunction = MySQLSource.<String>builder()
+                .hostname("hadoop102")
+                .port(3306)
+                .username("root")
+                .password("000000")
+                .databaseList("gmall_flink_0526")
+                .startupOptions(StartupOptions.latest())
+                .deserializer(new MyDeseriali())
+                .build();
+
+        //将flinkCDC添加到source中
+        DataStreamSource<String> streamSource = env.addSource(sourceFunction);
+
+        //将数据发送至kafka
+        streamSource.addSink(MyKafkaUtil.getKafkaSink("ods_base_db"));
+
+        env.execute();
+    }
+
+}
